25c846cdca69cb4a5470d0c73ffd6cb665c4f1e9
zzq <2455676683@qq.com>
2017-12-25 10:43:18 +0800

    yoyiyi

---
+++ app/src/main/java/com/yoyiyi/soleil/App.kt
@@ -0,0 +1,18 @@
+package com.yoyiyi.soleil
+
+import android.app.Application
+
+/**
+ * 类名：App
+ * 描述：TODO
+ * 时间：2017/12/25 10:36
+ *
+ * @author <a href="2455676683@qq.com">zzq</a>
+ * @version 1.0
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
